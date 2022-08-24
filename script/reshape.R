32390fc013f6c09ebab69029c7129d52fbbed146
Dennis Angemi <dennisangemi@gmail.com>
2022-08-24 13:59:49 +0200

    rm trailing ws

--- script/reshape.R
+++ script/reshape.R
@@ -10,13 +10,13 @@ relic <- read_csv(here("data/tests/relic.csv"))
 
 # reshaping
 long <- df %>%
-    rename(item_source.pd = pd_source, 
-           item_source.avs = avs_source, 
-           item_description.pd = pd_description, 
-           item_description.avs = avs_description, 
-           item_endorsement.pd = pd_endorsement, 
+    rename(item_source.pd = pd_source,
+           item_source.avs = avs_source,
+           item_description.pd = pd_description,
+           item_description.avs = avs_description,
+           item_endorsement.pd = pd_endorsement,
            item_endorsement.avs = avs_endorsement) %>%
-    pivot_longer(cols = starts_with("item"), 
+    pivot_longer(cols = starts_with("item"),
           names_to = c(".value", "field"), names_sep = "_") %>%
     separate(field, into = c("type", "list"), sep = "\\.") %>%
     spread(type, item) %>%
